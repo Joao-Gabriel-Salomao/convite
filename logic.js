8b60b7c1926f8dea6a96cc98bf3311f27cb72663
Jonh <89265142+Joao-Gabriel-Salomao@users.noreply.github.com>
2023-05-24 15:26:05 -0200

    hy

---
+++ logic.js
@@ -0,0 +1,13 @@
+
+
+
+function aceitou() {
+    window.alert("YESSSS! RÁPIDO! TIRE PRINT PARA EU SABER A RESPOSTA!");
+}
+
+function recusou() {
+    window.alert("ERROR ERROR! TRY AGAIN");
+}
+
+
+// window.location.href = "http://www.devmedia.com.br";
